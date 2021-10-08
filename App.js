d9a0861a90ace3c81e19baedf8be5a9b89bac13f
Alex Shtromberg <alexs@tailormed.co>
2021-10-08 15:04:55 +0300

    idents change

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
-import { useColorScheme, StyleSheet, Text, View, StatusBar, Alert, TouchableOpacity } from 'react-native';
-import Matter from "matter-js";
-import { GameEngine } from "react-native-game-engine";
+import {
+  useColorScheme,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Alert,
+  TouchableOpacity,
+} from 'react-native';
+import Matter from 'matter-js';
+import { GameEngine } from 'react-native-game-engine';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -17,25 +25,25 @@ export const randomBetween = (min, max) => {
 };
 
 export const generatePipes = () => {
-  let topPipeHeight = randomBetween(100, (Constants.MAX_HEIGHT / 2) - 100);
-  let bottomPipeHeight = Constants.MAX_HEIGHT - topPipeHeight - Constants.GAP_SIZE;
+  let topPipeHeight = randomBetween(100, Constants.MAX_HEIGHT / 2 - 100);
+  let bottomPipeHeight =
+    Constants.MAX_HEIGHT - topPipeHeight - Constants.GAP_SIZE;
 
-  let sizes = [topPipeHeight, bottomPipeHeight]
+  let sizes = [topPipeHeight, bottomPipeHeight];
 
   if (Math.random() < 0.5) {
     sizes = sizes.reverse();
   }
 
-
   return sizes;
-}
+};
 
 class HomeScreen extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      running: true
+      running: true,
     };
 
     this.gameEngine = null;
@@ -48,62 +56,134 @@ class HomeScreen extends Component {
     let world = engine.world;
     world.gravity.y = 1.2;
 
-    let bird = Matter.Bodies.rectangle(Constants.MAX_WIDTH / 4, Constants.MAX_HEIGHT / 2, 50, 50);
+    let bird = Matter.Bodies.rectangle(
+      Constants.MAX_WIDTH / 4,
+      Constants.MAX_HEIGHT / 2,
+      50,
+      50,
+    );
     bird.restitution = 20;
-    let floor = Matter.Bodies.rectangle(Constants.MAX_WIDTH / 2, Constants.MAX_HEIGHT - 25, Constants.MAX_WIDTH, 50, { isStatic: true });
-    let ceiling = Matter.Bodies.rectangle(Constants.MAX_WIDTH / 2, 25, Constants.MAX_WIDTH, 50, { isStatic: true });
+    let floor = Matter.Bodies.rectangle(
+      Constants.MAX_WIDTH / 2,
+      Constants.MAX_HEIGHT - 25,
+      Constants.MAX_WIDTH,
+      50,
+      { isStatic: true },
+    );
+    let ceiling = Matter.Bodies.rectangle(
+      Constants.MAX_WIDTH / 2,
+      25,
+      Constants.MAX_WIDTH,
+      50,
+      { isStatic: true },
+    );
 
     let [pipe1Height, pipe2Height] = generatePipes();
 
-    let pipe1 = Matter.Bodies.rectangle(Constants.MAX_WIDTH - (Constants.PIPE_WIDTH / 2), pipe1Height / 2, Constants.PIPE_WIDTH, pipe1Height, { isStatic: true });
-    let pipe2 = Matter.Bodies.rectangle(Constants.MAX_WIDTH - (Constants.PIPE_WIDTH / 2), Constants.MAX_HEIGHT - (pipe2Height / 2), Constants.PIPE_WIDTH, pipe2Height, { isStatic: true });
+    let pipe1 = Matter.Bodies.rectangle(
+      Constants.MAX_WIDTH - Constants.PIPE_WIDTH / 2,
+      pipe1Height / 2,
+      Constants.PIPE_WIDTH,
+      pipe1Height,
+      { isStatic: true },
+    );
+    let pipe2 = Matter.Bodies.rectangle(
+      Constants.MAX_WIDTH - Constants.PIPE_WIDTH / 2,
+      Constants.MAX_HEIGHT - pipe2Height / 2,
+      Constants.PIPE_WIDTH,
+      pipe2Height,
+      { isStatic: true },
+    );
 
     let [pipe3Height, pipe4Height] = generatePipes();
 
-    let pipe3 = Matter.Bodies.rectangle(Constants.MAX_WIDTH * 2 - (Constants.PIPE_WIDTH / 2), pipe3Height / 2, Constants.PIPE_WIDTH, pipe3Height, { isStatic: true });
-    let pipe4 = Matter.Bodies.rectangle(Constants.MAX_WIDTH * 2 - (Constants.PIPE_WIDTH / 2), Constants.MAX_HEIGHT - (pipe4Height / 2), Constants.PIPE_WIDTH, pipe4Height, { isStatic: true });
+    let pipe3 = Matter.Bodies.rectangle(
+      Constants.MAX_WIDTH * 2 - Constants.PIPE_WIDTH / 2,
+      pipe3Height / 2,
+      Constants.PIPE_WIDTH,
+      pipe3Height,
+      { isStatic: true },
+    );
+    let pipe4 = Matter.Bodies.rectangle(
+      Constants.MAX_WIDTH * 2 - Constants.PIPE_WIDTH / 2,
+      Constants.MAX_HEIGHT - pipe4Height / 2,
+      Constants.PIPE_WIDTH,
+      pipe4Height,
+      { isStatic: true },
+    );
 
     Matter.World.add(world, [bird, floor, ceiling, pipe1, pipe2, pipe3, pipe4]);
-    Matter.Events.on(engine, 'collisionStart', (event) => {
+    Matter.Events.on(engine, 'collisionStart', event => {
       var pairs = event.pairs;
 
-      this.gameEngine.dispatch({ type: "game-over" });
-
+      this.gameEngine.dispatch({ type: 'game-over' });
     });
 
     return {
       physics: { engine: engine, world: world },
-      floor: { body: floor, size: [Constants.MAX_WIDTH, 50], color: "green", renderer: Wall },
-      ceiling: { body: ceiling, size: [Constants.MAX_WIDTH, 50], color: "green", renderer: Wall },
+      floor: {
+        body: floor,
+        size: [Constants.MAX_WIDTH, 50],
+        color: 'green',
+        renderer: Wall,
+      },
+      ceiling: {
+        body: ceiling,
+        size: [Constants.MAX_WIDTH, 50],
+        color: 'green',
+        renderer: Wall,
+      },
       bird: { body: bird, size: [50, 50], color: 'red', renderer: Bird },
-      pipe1: { body: pipe1, size: [Constants.PIPE_WIDTH, pipe1Height], color: "green", renderer: Wall },
-      pipe2: { body: pipe2, size: [Constants.PIPE_WIDTH, pipe2Height], color: "green", renderer: Wall },
-      pipe3: { body: pipe3, size: [Constants.PIPE_WIDTH, pipe3Height], color: "green", renderer: Wall },
-      pipe4: { body: pipe4, size: [Constants.PIPE_WIDTH, pipe4Height], color: "green", renderer: Wall }
-    }
-  }
+      pipe1: {
+        body: pipe1,
+        size: [Constants.PIPE_WIDTH, pipe1Height],
+        color: 'green',
+        renderer: Wall,
+      },
+      pipe2: {
+        body: pipe2,
+        size: [Constants.PIPE_WIDTH, pipe2Height],
+        color: 'green',
+        renderer: Wall,
+      },
+      pipe3: {
+        body: pipe3,
+        size: [Constants.PIPE_WIDTH, pipe3Height],
+        color: 'green',
+        renderer: Wall,
+      },
+      pipe4: {
+        body: pipe4,
+        size: [Constants.PIPE_WIDTH, pipe4Height],
+        color: 'green',
+        renderer: Wall,
+      },
+    };
+  };
 
-  onEvent = (e) => {
-    if (e.type === "game-over") {
+  onEvent = e => {
+    if (e.type === 'game-over') {
       //Alert.alert("Game Over");
       this.setState({
-        running: false
+        running: false,
       });
     }
-  }
+  };
 
   reset = () => {
     this.gameEngine.swap(this.setupWorld());
     this.setState({
-      running: true
+      running: true,
     });
-  }
+  };
 
   render() {
     return (
       <View style={styles.container}>
         <GameEngine
-          ref={(ref) => { this.gameEngine = ref; }}
+          ref={ref => {
+            this.gameEngine = ref;
+          }}
           style={styles.gameContainer}
           systems={[Physics]}
           running={this.state.running}
@@ -111,11 +191,15 @@ class HomeScreen extends Component {
           entities={this.entities}>
           <StatusBar hidden={true} />
         </GameEngine>
-        {!this.state.running && <TouchableOpacity style={styles.fullScreenButton} onPress={this.reset}>
-          <View style={styles.fullScreen}>
-            <Text style={styles.gameOverText}>Game Over</Text>
-          </View>
-        </TouchableOpacity>}
+        {!this.state.running && (
+          <TouchableOpacity
+            style={styles.fullScreenButton}
+            onPress={this.reset}>
+            <View style={styles.fullScreen}>
+              <Text style={styles.gameOverText}>Game Over</Text>
+            </View>
+          </TouchableOpacity>
+        )}
       </View>
     );
   }
@@ -135,7 +219,7 @@ const styles = StyleSheet.create({
   },
   gameOverText: {
     color: 'white',
-    fontSize: 48
+    fontSize: 48,
   },
   fullScreen: {
     position: 'absolute',
@@ -146,7 +230,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'black',
     opacity: 0.8,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   fullScreenButton: {
     position: 'absolute',
@@ -154,8 +238,8 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     right: 0,
-    flex: 1
-  }
+    flex: 1,
+  },
 });
 
 const App = () => {
--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,4 @@
-import {Dimensions} from 'react-native';
+import { Dimensions } from 'react-native';
 
 export default {
   MAX_WIDTH: Dimensions.get('screen').width,

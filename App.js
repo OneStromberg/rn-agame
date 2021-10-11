30e16e6c81f03e4d19bba149db63afc81c3c8bb2
Alex Shtromberg <alexs@tailormed.co>
2021-10-12 00:06:31 +0300

    Added ads

--- App.js
+++ App.js
@@ -1,13 +1,54 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Button, Text, TouchableOpacity, View } from 'react-native';
 
 import HomeScreen from './src/Home';
 
-// import './src/admod';
+import { InterstitialAd, AdEventType, TestIds } from '@react-native-firebase/admob';
+
+const adUnitId = __DEV__ ? TestIds.INTERSTITIAL : 'ca-app-pub-4252623491838002~3261772449';
+
+const interstitial = InterstitialAd.createForAdRequest(adUnitId, {
+  requestNonPersonalizedAdsOnly: true,
+  keywords: ['fashion', 'clothing'],
+});
 
 const Stack = createNativeStackNavigator();
+
+const AdsTest = () => {
+  const [loaded, setLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    const eventListener = interstitial.onAdEvent(type => {
+      if (type === AdEventType.LOADED) {
+        setLoaded(true);
+      }
+    });
+
+    // Start loading the interstitial straight away
+    interstitial.load();
+
+    // Unsubscribe from events on unmount
+    return () => {
+      eventListener();
+    };
+  }, []);
+
+  // No advert ready to show yet
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <Button
+      title="Show Interstitial"
+      onPress={() => {
+        interstitial.show();
+      }}
+    />
+  );
+}
 
 const MainScreen = ({ navigation }) => (
   <View>
@@ -16,11 +57,12 @@ const MainScreen = ({ navigation }) => (
         <Text>Play</Text>
       </View>
     </TouchableOpacity>
-    <TouchableOpacity onPress={() => {}}>
+    <TouchableOpacity onPress={() => { }}>
       <View>
         <Text>Score</Text>
       </View>
     </TouchableOpacity>
+    <AdsTest />
   </View>
 )
 
